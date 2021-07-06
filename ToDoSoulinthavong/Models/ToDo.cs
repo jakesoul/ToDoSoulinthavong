90e7afa4964d6ad217a1f23028ff28a6d666c23d
jakesoul <mythicalpickle6@gmail.com>
2021-07-05 21:35:18 -0500

    Add project files.

---
+++ ToDoSoulinthavong/Models/ToDo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoSoulinthavong.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description.")]
+        public string Description { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter a due date.")]
+        public DateTime? DueDate { get; set; }
+
+        [Required(ErrorMessage = "Please enter a category.")]
+        public string CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        [Required(ErrorMessage = "Please enter a status.")]
+        public string StatusId { get; set; }
+        public Status Status { get; set; }
+
+        public bool Overdue => Status?.Name.ToLower() == "open" && DueDate < DateTime.Today;
+
+    }
+}
